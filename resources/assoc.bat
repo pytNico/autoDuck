926320a085e7fe9482fb48aded943da7e2809ab4
pytNico <83766945+pytNico@users.noreply.github.com>
2022-02-23 11:01:13 +0100

    Add files via upload

---
+++ resources/assoc.bat
@@ -0,0 +1,4 @@
+@echo off
+C:\SippicomInstall\SetUserFTA.exe C:\SippicomInstall\assoc\VLCassoc.txt
+C:\SippicomInstall\SetUserFTA.exe C:\SippicomInstall\assoc\Acroassoc.txt
+C:\SippicomInstall\SetUserFTA.exe C:\SippicomInstall\assoc\Officeassoc.txt
