2f6582f2a553164cbdfa5d8ca380bba051975e3f
pytNico <83766945+pytNico@users.noreply.github.com>
2022-02-23 10:58:40 +0100

    Update autoDuck.bat

--- autoDuck.bat
+++ autoDuck.bat
@@ -2,5 +2,5 @@
 if EXIST "%temp%\autoDuck.ps1" (
 del %temp%\autoDuck.ps1
 )
-powershell -noprofile -Command "iwr https://raw.githubusercontent.com/pytNico/autoDuck/master/autoDuck.ps1 -OutFile %temp%\autoDuck.ps1"
+powershell -noprofile -Command "iwr https://raw.githubusercontent.com/pytNico/autoDuck/main/autoDuck.ps1 -OutFile %temp%\autoDuck.ps1"
 powershell -NoProfile -Command "& {Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%temp%\autoDuck.ps1""' -Verb RunAs}"
